514ac6b5e389e1f472eca6153ac301ca41f133f6
Hüseyin Öztürk <b_dd0_huseyin_06@outlook.com>
2022-12-13 00:27:37 +0300

    AdminPage
    -Yetenekler Sayfası Eklendi Düzeltildi
    -Dashboard Sayfası Eklendi

---
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
