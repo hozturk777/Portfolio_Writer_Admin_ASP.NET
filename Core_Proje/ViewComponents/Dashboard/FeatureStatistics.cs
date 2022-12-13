af067c055bb9c5651f588ed86bc18d7ccd8ad4e6
Hüseyin Öztürk <b_dd0_huseyin_06@outlook.com>
2022-12-13 22:37:39 +0300

    AdminPage
    -Feature Sayfası Eklendi

--- Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Linq;
 
 namespace Core_Proje.ViewComponents.Dashboard
 {
     public class FeatureStatistics : ViewComponent
     {
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = c.Skills.Count();
+            ViewBag.v2 = c.Messages.Where(x => x.Status == false).Count();
+            ViewBag.v3 = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.v4 = c.Experiences.Count();
             return View();
         }
     }
