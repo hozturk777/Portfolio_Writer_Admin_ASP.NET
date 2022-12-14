1f7fef1403cfa9160f2c993663fdece5f20dcbe7
Hüseyin Öztürk <b_dd0_huseyin_06@outlook.com>
2022-12-14 14:25:24 +0300

    AdminPage
    -Proje Listesi Eklendi

---
+++ Core_Proje/ViewComponents/Dashboard/ProjectList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class ProjectList : ViewComponent
+    {
+        PortFolioManager portFolio = new PortFolioManager(new EfPortFolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portFolio.TGetList();
+            return View(values);
+        }
+    }
+}
