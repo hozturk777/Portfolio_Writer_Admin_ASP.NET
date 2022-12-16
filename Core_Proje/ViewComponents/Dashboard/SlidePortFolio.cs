8b14f21cdbe60370c5d223eeee7c09ed74b8730a
Hüseyin Öztürk <b_dd0_huseyin_06@outlook.com>
2022-12-16 19:51:57 +0300

    AdminPage
    -Dashboard portfolio slide eklendi
    -Dashboard gelenson 5 mesaj gösterimi eklendi

---
+++ Core_Proje/ViewComponents/Dashboard/SlidePortFolio.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class SlidePortFolio : ViewComponent
+    {
+        PortFolioManager portFolioManager = new PortFolioManager(new EfPortFolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portFolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
