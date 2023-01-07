e57bf4791f5298d27c13f01648f1160cc7777862
Hüseyin Öztürk <100473592+hozturk777@users.noreply.github.com>
2023-01-07 16:37:36 +0300

    AdminPage
    -Admin sayfası mesaj gönderme eklendi
    -İyileştirmeler yapıldı

---
+++ Core_Proje/Controllers/AdminMessageController.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Core_Proje.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IActionResult ReceiverMessageList()
+        {
+            string p;
+            p = "admin@gmail.com";
+            var values = writerMessageManager.GetListReceiverMessage(p);
+            return View(values);
+        }
+        public IActionResult SenderMessageList()
+        {
+            string p;
+            p = "admin@gmail.com";
+            var values = writerMessageManager.GetListSenderMessage(p);
+            return View(values);
+        }
+        public IActionResult AdminMessageDetails(int id)
+        {
+           
+            var values = writerMessageManager.TGetByID(id);
+            return View(values);
+        }
+        public IActionResult SenderMessageDelete(int id)
+        {
+            var values = writerMessageManager.TGetByID(id);
+            writerMessageManager.TDelete(values);
+            return RedirectToAction("SenderMessageList");
+        }
+        public IActionResult ReceiverMessageDelete(int id)
+        {
+            var values = writerMessageManager.TGetByID(id);
+            writerMessageManager.TDelete(values);
+            return RedirectToAction("ReceiverMessageList");
+        }
+        [HttpGet]
+        public IActionResult AdminMessageSend()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AdminMessageSend(WriterMessage writerMessage)
+        {
+            writerMessage.Sender = "admin@gmail.com";
+            writerMessage.SenderName = "Admin";
+            writerMessage.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            Context c = new Context();
+            var usernamesurname = c.Users.Where(x => x.Email == writerMessage.Receiver).Select(y => y.Name+" "+y.Surname).FirstOrDefault();
+            writerMessage.ReceiverName = usernamesurname;
+            writerMessageManager.TAdd(writerMessage);
+            return RedirectToAction("SenderMessageList");
+        }
+    }
+}
