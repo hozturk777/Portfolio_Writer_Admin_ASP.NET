fd30e0ca81f4eaf88782f01dee31a3a12f818f01
Hüseyin Öztürk <100473592+hozturk777@users.noreply.github.com>
2023-01-11 18:20:40 +0300

    AdminPage
    -Sosyal medya ekleme sayfası eklendi

---
+++ Core_Proje/Controllers/SocialMediaController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core_Proje.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IActionResult Index()
+        {
+            
+            var values = socialMediaManager.TGetList();
+
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMedia socialMedia)
+        {
+            socialMedia.Status = true;
+            socialMediaManager.TAdd(socialMedia);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateSocialMedia(int id)
+        {
+            var values = socialMediaManager.TGetByID(id);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UpdateSocialMedia(SocialMedia socialMedia)
+        {
+            socialMediaManager.TUpdate(socialMedia);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var values = socialMediaManager.TGetByID(id);
+            socialMediaManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+    }
+}
